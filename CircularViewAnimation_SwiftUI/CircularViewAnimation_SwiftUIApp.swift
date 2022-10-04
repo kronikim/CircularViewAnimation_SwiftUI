0d93638d9785c38276ae92483d6772b1953180d0
deniz karahan <denizkarahan88@gmail.com>
2022-10-05 00:23:45 +0200

    Initial Commit

---
+++ CircularViewAnimation_SwiftUI/CircularViewAnimation_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CircularViewAnimation_SwiftUIApp.swift
+//  CircularViewAnimation_SwiftUI
+//
+//  Created by deniz karahan on 5.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct CircularViewAnimation_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
